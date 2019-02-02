8de336915a7772fb6663b05f87c0a29ca3384eb0
SofiCrazy <shelestun98@list.ru>
2019-02-03 00:37:44 +0200

    added hello Nikita

---
+++ task1/main.js
@@ -0,0 +1,7 @@
+var button = document.createElement('input');
+var image = new Image();
+image.src = 'http://adhoc-coaching.spb.ru/wp-content/uploads/Синее.jpg';
+
+function clikcMouse(Event) {
+    addEventListener("click", button);
+
