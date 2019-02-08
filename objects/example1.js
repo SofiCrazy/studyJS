644eb3231bf6cdbeb004df4787de9afa7208b86f
Sofi <s.d.crazy.ru@yandex.ru>
2019-02-09 00:41:08 +0200

    added new objects

--- objects/example1.js
+++ objects/example1.js
@@ -1,14 +1,40 @@
 var cat = {
-    name: 'Васька',
-    age: '5',
+    name: 'Vasya',
+    age: 5,
     color: 'black',
+    satiety: 0,
+    life: 9,
     getInfo: function () {
         console.log('\n');
         return `Name: ${this.name}\nAge: ${this.age}\nColor: ${this.color}`
     }
 };
 
-console.log(cat.getInfo());
-cat.name = 'Барсик';
+var fish = {
+    size: 2,
+    power: 1,
+};
 
-console.log(cat.getInfo());
+var bird = {
+    size: 1,
+    power: 3,
+};
+
+var mouse = {
+    size: 1,
+};
+cat.eat = function (food) {
+    if (food.power !== undefined) {
+        this.life = this.life - food.power;
+    }
+    this.satiety = this.satiety + food.size;
+};
+console.log(cat.life);
+cat.eat(fish);
+console.log(cat.life);
+cat.eat(bird);
+console.log(cat.life);
+cat.eat(mouse);
+console.log(cat.life);
+
+
