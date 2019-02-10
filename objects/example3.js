051756726c3855c8bae8c59e6928765da3aed84f
Sofi <s.d.crazy.ru@yandex.ru>
2019-02-10 23:45:04 +0200

    this code is worker.

--- objects/example3.js
+++ objects/example3.js
@@ -2,7 +2,7 @@ let warrior1 = {
     name: 'Zigs',
     class: 'wizard',
     life: 10,
-    damage: 3,
+    damage: 2,
 };
 
 let warrior2 = {
@@ -12,25 +12,29 @@ let warrior2 = {
     life: 12,
 };
 
+
 console.log(warrior1.life, warrior1.name);
 console.log(warrior2.life, warrior2.name);
 
 function figth(warrior) {
-    while (this.life !== 0) {
+    while (this.life > 0) {
         this.life = this.life - warrior.damage;
         warrior.life = warrior.life - this.damage;
         console.log(warrior.life, warrior.name);
         console.log(this.life, this.name);
-        if (this.life == 0) {
-            console.log("Zigs is winner")
+        if (this.life > 0 && warrior.life <= 0) {
+            console.log(this.name + " is winner ")
+        } else {
+            console.log(warrior.name + " is winner ")
         }
     }
 
 };
 warrior1.attack = figth;
 warrior2.attack = figth;
-warrior1.attack(warrior2);
 
+warrior1.attack(warrior2);
 console.log(warrior1.life, warrior1.name);
 console.log(warrior2.life, warrior2.name);
+console.clear();
 
