406b64b2a0ec0216b7aa3a399d2ae799deca1fd5
Sofi <s.d.crazy.ru@yandex.ru>
2019-02-10 23:12:33 +0200

    fight between warrior1 and warrior2.

--- objects/example3.js
+++ objects/example3.js
@@ -12,13 +12,23 @@ var warrior2 = {
     life: 12,
 };
 
+console.log(warrior1.life, warrior1.name);
+console.log(warrior2.life, warrior2.name);
+
 function figth(warrior) {
     while (this.life > 0) {
         this.life = this.life - warrior.damage;
+        warrior.life = warrior.life - this.damage;
+        console.log(warrior.life, warrior.name);
+        console.log(this.life, this.name)
     }
-}
 
-warrior1.attack = figth(warrior2);
+
+};
+warrior1.attack = figth;
+warrior2.attack = figth;
+warrior1.attack(warrior2);
+
 console.log(warrior1.life, warrior1.name);
-
+console.log(warrior2.life, warrior2.name);
 
