f922a76a0a766d3b53282a914c91e218ebaa7f73
Sofi <s.d.crazy.ru@yandex.ru>
2019-02-10 19:48:11 +0200

    fight between warrior1 and warrior2.

---
+++ objects/example3.js
@@ -0,0 +1,25 @@
+var warrior1 = {
+    name: 'Zigs',
+    class: 'wizard',
+    life: 10,
+    damage: 5,
+};
+
+var warrior2 = {
+    name: 'Jinx',
+    class: 'adk',
+    damage: 2,
+    life: 12,
+};
+
+function figth(warrior) {
+    if (this.life !== undefined) {
+        this.life = this.life - warrior.damage;
+    }
+}
+
+warrior1 = fight(warrior2);
+warrior2 = fight(warrior1);
+
+console.log(warrior1.life, warrior1.name);
+console.log(warrior2.life, warrior2.name);
