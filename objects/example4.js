ac25471f58b9698b3f29ba168271cf9eb3278a14
Sofi <s.d.crazy.ru@yandex.ru>
2019-02-14 23:59:23 +0200

    commit 2

--- objects/example4.js
+++ objects/example4.js
@@ -1,23 +1,19 @@
 const zigs = {
-    name: 'zigs',
+    name: 'Zigs',
     class: 'wizard',
     life: 7,
     damage: 1,
+    armor: 2,
 };
 
 const jinx = {
     name: 'Jinx',
     class: 'adk',
     damage: 1,
-    life: 6,
+    life: 7,
+    armor: 3,
 };
 
-const raptor = {
-    name: 'Raptor',
-    class: 'Tank',
-    damage: 1,
-    life: 5,
-};
 function isLife() {
     if (this.life > 0) {
         return true;
@@ -28,21 +24,25 @@ function isLife() {
 
 function fight(warrior) {
     console.log("The " + this.name + " fighting with " + warrior.name);
+    this.life = this.life + this.armor;
+    warrior.life = warrior.life - this.armor;
     while (this.isLife() && warrior.isLife()) {
         this.life = this.life - warrior.damage;
         warrior.life = warrior.life - this.damage;
         console.log(warrior.life, warrior.name);
         console.log(this.life, this.name);
     }
-    if (this.isLife()) {
-        console.log(this.name + " is winner")
-    } else {
-        console.log(this.name + " is loser")
+    if (!this.isLife() && !warrior.isLife()) {
+        console.log("It is a draw!!!");
+        return
     }
-    if (warrior.isLife()) {
-        console.log(warrior.name + " is winner")
-    } else {
-        console.log(warrior.name + "is a loser")
+    if (!this.isLife()) {
+        console.log(this.name + " is a loser");
+        console.log(warrior.name + " is a winner")
+    }
+    if (!warrior.isLife()) {
+        console.log(warrior.name + " is a loser");
+        console.log(this.name + " is a winner")
     }
 
 }
@@ -51,9 +51,6 @@ zigs.attack = fight;
 jinx.attack = fight;
 zigs.isLife = isLife;
 jinx.isLife = isLife;
-raptor.isLife = isLife;
 
 zigs.attack(jinx);
 console.log('\n==========\n');
-jinx.attack(raptor);
-raptor.attack = fight;
